c0ff1c22807577e4a92b89cc50772384a543ac8b
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2024-11-28 23:31:44 +0100

    create a Player component that represent a player item in the list of players (not in the soccer pitch)

--- assets/components/Player.js
+++ assets/components/Player.js
@@ -1,12 +1,24 @@
 export default function (player) {
-	return `<li class="player flex justify-between items-center gap-2 p-2 border-b border-gray-500">
-					<img class="avatar max-w-8 xs:max-w-10" src="${player.photo}" alt="" />
-					<h4 class="player-name text-[clamp(7px,1vw,14px)]">${player.name}</h4>
-					<ul class="logos  flex justify-center items-center gap-2 [&>li]:w-5">
-						<li class="flag"><img src="${player.flag}" alt="" /></li>
-						<li class="logo"><img src="${player.logo}" alt="" /></li>
-						<li class="rating font-medium text-[8px] rounded-full leading-none aspect-square flex justify-center items-center bg-yellow-950 text-white" title="rating">${player.rating}</li>
-					</ul>
-				</li>
+	return `<li onclick="choosePlayer(event)" class="player flex justify-between items-center gap-2 p-2 border-b border-gray-500 relative cursor-pointer hover:bg-gray-400 transition-colors active:scale-95" id="${player.id}">
+				<div class="options absolute top-0.5 right-0.5 leading-[0]">
+					<i
+						onclick="openEditForm(${player.id})"
+						class="fa-regular fa-pen-to-square text-[.75rem] hover:scale-125 transition-transform cursor-pointer"></i>
+					<i
+						onclick="deletePlayer(${player.id})"
+						class="delete-icon fa-solid fa-xmark text-sm hover:scale-125 transition-transform cursor-pointer"></i>
+				</div>
+				<img class="avatar max-w-8 xs:max-w-10 aspect-square object-cover" src="${player.photo}" alt="" />
+				<h4 class="player-name text-[clamp(7px,1vw,14px)]">${player.name}</h4>
+				<ul class="logos flex justify-center items-center gap-2 [&>li]:w-5">
+					<li class="flag"><img src="${player.flag}" alt="" /></li>
+					<li class="logo"><img src="${player.logo}" alt="" /></li>
+					<li
+						class="rating font-medium text-[8px] rounded-full leading-none aspect-square flex justify-center items-center bg-yellow-950 text-white"
+						title="rating">
+						${player.rating}
+					</li>
+				</ul>
+			</li>
    `;
 }
